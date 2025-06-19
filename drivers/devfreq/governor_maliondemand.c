46ee164e93a185ec0cea0a060dc18d80e35e54e9
Meticulus <theonejohnnyd@gmail.com>
2025-06-19 16:19:11 +0200

    drivers: devfreq: Assign corrected `no_vsync` values now
    Signed-off-by: schwienernitzel <pfelix0803@gmail.com>

--- drivers/devfreq/governor_maliondemand.c
+++ drivers/devfreq/governor_maliondemand.c
@@ -129,6 +129,13 @@ static int devfreq_mali_ondemand_func(struct devfreq *df,
 		data->vsync_downdifferential = DFMO_VSYNC_DOWNDIFFERENCTIAL;
 		data->no_vsync_upthreshold = DFMO_NO_VSYNC_UPTHRESHOLD;
 		data->no_vsync_downdifferential = DFMO_NO_VSYNC_DOWNDIFFERENCTIAL;
+		if (data->vsync) {
+			dfso_upthreshold = data->vsync_upthreshold;
+			dfso_downdifferential = data->vsync_downdifferential;
+		} else {
+			dfso_upthreshold = data->no_vsync_upthreshold;
+			dfso_downdifferential = data->no_vsync_downdifferential;
+		}
 		pr_err("Meticulus: Resetting performance parameters to default\n");
 	}
 
